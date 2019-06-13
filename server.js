accf814a47eb0e4b761a7efe0dcc6afc0f3ba33c
Piotr Jachowicz <Piotr.Jachowicz@santanderconsumer.dk>
2019-06-13 08:47:22 +0200

    Port number for azure

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const http = require('http');
 
 app.use(cors());
+app.configure(function () {
+  app.set('port', process.env.PORT || 3000);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
