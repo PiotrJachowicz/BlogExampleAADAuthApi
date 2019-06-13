9b0a5b464b4b44dab04402a76b2a69cceb679b64
Piotr Jachowicz <Piotr.Jachowicz@santanderconsumer.dk>
2019-06-13 08:17:48 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'))
+});
